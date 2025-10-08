b72bbf2182a325e177266d3fa3c7610bb97a23b1
Kingkonbro <konradboruch735@gmail.com>
2025-10-08 18:57:04 +0100

    Not working

---
+++ Challenge_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The user is asked to enter a number
+            Console.Write("Input a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                if ( ( (i % 3)==0 ) && ( (i % 5) == 0 ) )
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                if ((i % 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if ((i % 3) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+    }
+}
