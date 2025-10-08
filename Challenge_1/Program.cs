7f86c6492cba722955b61a4350b0cd1a19b44d94
Kingkonbro <konradboruch735@gmail.com>
2025-10-08 10:47:35 +0100

    Add project files.

---
+++ Challenge_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Challenge_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // A for loop is created to count down from 10 to 0
+            for (int i = 10; i > 0; i-- )
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Lift-off!");
+        }   
+    }
+}
