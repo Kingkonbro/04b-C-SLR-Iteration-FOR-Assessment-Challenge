e6e3d3858dcd062f2e99c554594c3496ea3bbf85
Kingkonbro <konradboruch735@gmail.com>
2025-10-08 11:30:00 +0100

    Not Completed

---
+++ Challenge_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The user is aksed to enter a number and stored in the variable num
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int factorialResult = num ;
+
+            for (int i = num; i > 0; i--)
+            {
+                Console.WriteLine(i);
+                //Console.WriteLine(factorialResult);
+                factorialResult = factorialResult * i ;
+            }
+            Console.WriteLine(factorialResult);
+        }
+    }
+}
